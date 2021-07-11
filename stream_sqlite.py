79a0bee454d232ed7838e57bda9e803f6003135e
Michal Charemza <michal@charemza.name>
2021-07-11 12:59:18 +0100

    feat: iterator that just yields the bytes of a sqlite file

--- stream_sqlite.py
+++ stream_sqlite.py
@@ -1,3 +1,4 @@
 
-def stream_sqlite():
-	pass
+def stream_sqlite(sqlite_chunks):
+	for chunk in sqlite_chunks:
+		yield chunk
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import sqlite3
+import tempfile
 import unittest
 
 from stream_sqlite import stream_sqlite
@@ -6,4 +8,22 @@ from stream_sqlite import stream_sqlite
 class TestStreamSqlite(unittest.TestCase):
 
     def test_stream_sqlite(self):
-        stream_sqlite()
+        for chunk in stream_sqlite(small_db_bytes()):
+            pass
+
+def small_db_bytes():
+    with tempfile.NamedTemporaryFile() as fp:
+        with sqlite3.connect(fp.name) as con:
+            cur = con.cursor()
+            cur.execute('''
+                CREATE TABLE my_table (my_text_col text)
+            ''')
+            cur.execute("INSERT INTO my_table VALUES ('some-text')")
+            con.commit()
+
+        with open(fp.name, 'rb') as f:
+            while True:
+                chunk = f.read(1)
+                if not chunk:
+                    break
+                yield chunk
