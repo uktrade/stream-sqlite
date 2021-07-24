562d40a9825c15e25d8ae3784bd45f26f2541294
Michal Charemza <michal@charemza.name>
2021-07-24 22:31:15 +0100

    tests: test larger table

--- test.py
+++ test.py
@@ -65,6 +65,22 @@ class TestStreamSqlite(unittest.TestCase):
                     [],
                 ) for i in range(2, 101)], all_chunks)
 
+    def test_large_table(self):
+        for chunk_size in [1, 2, 3, 5, 7, 32, 131072]:
+            with self.subTest(chunk_size=chunk_size):
+                sqls = [
+                    "CREATE TABLE my_table_1 (my_text_col_a text, my_text_col_b text);",
+                ] + [
+                    "INSERT INTO my_table_1 VALUES ('some-text-a', 'some-text-b')",
+                ] * 1000
+                chunks = stream_sqlite(db(sqls, chunk_size))
+                all_rows = flatten([chunk[2] for chunk in chunks])
+
+                self.assertEqual(
+                    [{'my_text_col_a': b'some-text-a', 'my_text_col_b': b'some-text-b'}] * 1000,
+                    all_rows,
+                )
+
 def db(sqls, chunk_size):
     with tempfile.NamedTemporaryFile() as fp:
         with sqlite3.connect(fp.name, isolation_level=None) as con:
@@ -78,3 +94,10 @@ def db(sqls, chunk_size):
                 if not chunk:
                     break
                 yield chunk
+
+def flatten(l):
+    return [
+        item
+        for sublist in l
+        for item in sublist
+    ]
