13d3795af1c55f813bf3801ecd789d0d7cb3d698
Michal Charemza <michal@charemza.name>
2021-07-24 16:33:00 +0100

    tests: test a variety of chunk sizes

--- test.py
+++ test.py
@@ -8,25 +8,27 @@ from stream_sqlite import stream_sqlite
 class TestStreamSqlite(unittest.TestCase):
 
     def test_stream_sqlite(self):
-        chunks = stream_sqlite(small_db_bytes())
-        all_chunks = [chunk for chunk in chunks]
-        self.assertEqual([(
-            b'my_table_1',
-            [
-                {'cid': 0, 'name': 'my_text_col_a', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
-                {'cid': 1, 'name': 'my_text_col_b', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
-            ],
-            [{'my_text_col_a': b'some-text-a', 'my_text_col_b': b'some-text-b'}],
-        ),(
-            b'my_table_2',
-            [
-                {'cid': 0, 'name': 'my_text_col_a', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
-                {'cid': 1, 'name': 'my_text_col_b', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
-            ],
-            [],
-        )], all_chunks)
+        for chunk_size in [1, 2, 3, 5, 7, 32, 131072]:
+            with self.subTest(chunk_size=chunk_size):
+                chunks = stream_sqlite(small_db_bytes(chunk_size))
+                all_chunks = [chunk for chunk in chunks]
+                self.assertEqual([(
+                    b'my_table_1',
+                    [
+                        {'cid': 0, 'name': 'my_text_col_a', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
+                        {'cid': 1, 'name': 'my_text_col_b', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
+                    ],
+                    [{'my_text_col_a': b'some-text-a', 'my_text_col_b': b'some-text-b'}],
+                ),(
+                    b'my_table_2',
+                    [
+                        {'cid': 0, 'name': 'my_text_col_a', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
+                        {'cid': 1, 'name': 'my_text_col_b', 'type': 'text', 'notnull': 0, 'dflt_value': None, 'pk': 0},
+                    ],
+                    [],
+                )], all_chunks)
 
-def small_db_bytes():
+def small_db_bytes(chunk_size):
     with tempfile.NamedTemporaryFile() as fp:
         with sqlite3.connect(fp.name) as con:
             cur = con.cursor()
@@ -41,7 +43,7 @@ def small_db_bytes():
 
         with open(fp.name, 'rb') as f:
             while True:
-                chunk = f.read(1)
+                chunk = f.read(chunk_size)
                 if not chunk:
                     break
                 yield chunk
